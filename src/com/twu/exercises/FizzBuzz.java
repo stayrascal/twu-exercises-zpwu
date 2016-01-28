8d4e4f7d98b084e75b53a3c5707dc82b6b41a6d6
rascal <stayrascal@gmail.com>
2016-01-28 20:19:06 +0800

    [ stayrascal ] add test case about print fizz or buzz

--- src/com/twu/exercises/FizzBuzz.java
+++ src/com/twu/exercises/FizzBuzz.java
@@ -1,5 +1,9 @@
 package com.twu.exercises;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
 public class FizzBuzz {
 
     private final Console console;
@@ -10,12 +14,29 @@ public class FizzBuzz {
 
 
     public void printNumber(int topNumber) {
-        while (topNumber-- > 0){
+        while (topNumber-- > 0) {
             printLine(String.valueOf(topNumber));
         }
     }
 
     private void printLine(String message) {
         console.print(message);
+    }
+
+
+    public void printNumberWithFizzAndBuzz(int topNumber) {
+        for (int i = 1; i <= topNumber; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    printLine("FizzBuzz");
+                } else {
+                    printLine("Fizz");
+                }
+            } else if (i % 5 == 0) {
+                printLine("Buzz");
+            } else {
+                printLine(String.valueOf(i));
+            }
+        }
     }
 }
--- test/com.twu.exercises/FizzBuzzTest.java
+++ test/com.twu.exercises/FizzBuzzTest.java
@@ -2,18 +2,20 @@ package com.twu.exercises;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class FizzBuzzTest {
 
     private Console console;
     private FizzBuzz fizzBuzz;
+    private InOrder inOrder;
 
     @Before
     public void setUp() throws Exception {
         console = mock(Console.class);
+        inOrder = inOrder(console);
         fizzBuzz = new FizzBuzz(console);
     }
 
@@ -29,6 +31,27 @@ public class FizzBuzzTest {
         verify(console).print("5");
         verify(console).print("6");
         verify(console).print("7");
+    }
+
+    @Test
+    public void the_fizz_or_buzz_should_replace_the_number_which_can_divisible_by_three_or_five_from_1_to_15() throws Exception {
+        fizzBuzz.printNumberWithFizzAndBuzz(15);
+
+        inOrder.verify(console, times(1)).print("1");
+        inOrder.verify(console, times(1)).print("2");
+        inOrder.verify(console, times(1)).print("Fizz");
+        inOrder.verify(console, times(1)).print("4");
+        inOrder.verify(console, times(1)).print("Buzz");
+        inOrder.verify(console, times(1)).print("Fizz");
+        inOrder.verify(console, times(1)).print("7");
+        inOrder.verify(console, times(1)).print("8");
+        inOrder.verify(console, times(1)).print("Fizz");
+        inOrder.verify(console, times(1)).print("Buzz");
+        inOrder.verify(console, times(1)).print("11");
+        inOrder.verify(console, times(1)).print("Fizz");
+        inOrder.verify(console, times(1)).print("13");
+        inOrder.verify(console, times(1)).print("14");
+        inOrder.verify(console, times(1)).print("FizzBuzz");
 
     }
 }
