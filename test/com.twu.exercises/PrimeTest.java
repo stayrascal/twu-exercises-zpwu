44a651bca121075c25be28c3de9b544c22952b52
rascal <stayrascal@gmail.com>
2016-01-28 21:16:15 +0800

    [ stayrascal ] add test case about get factors that contains same factor

--- test/com.twu.exercises/PrimeTest.java
+++ test/com.twu.exercises/PrimeTest.java
@@ -41,8 +41,20 @@ public class PrimeTest {
     public void shoud_return_a_list_which_size_is_one_when_factor_seven() throws Exception {
         Prime prime = new Prime();
         List<Integer> primeList = prime.generate(7);
+
         assertEquals(primeList.size(), 1);
         assertEquals(primeList.get(0), Integer.valueOf(7));
+    }
+
+    @Test
+    public void should_return_a_list_which_contains_2_3_3_when_factor_18() throws Exception {
+        Prime prime = new Prime();
+        List<Integer> primeList = prime.generate(18);
+
+        assertEquals(primeList.size(), 3);
+        assertEquals(primeList.get(0), Integer.valueOf(2));
+        assertEquals(primeList.get(1), Integer.valueOf(3));
+        assertEquals(primeList.get(2), Integer.valueOf(3));
 
     }
 }
